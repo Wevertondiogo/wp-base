9d138732a3d4806bd902e3ba2507e1256b0d3c01
Weverton procópio <wevertonprocopio690@gmail.com>
2021-02-09 18:26:14 -0300

    testing design of form login

--- frontend/src/app/login/login.module.ts
+++ frontend/src/app/login/login.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule,MatButtonModule],
   exports: [LoginComponent],
 })
 export class LoginModule {}
