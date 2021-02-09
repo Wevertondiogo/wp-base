07834631a4a037c66602aedf21acca595651cd7c
Weverton procópio <wevertonprocopio690@gmail.com>
2021-02-09 20:15:01 -0300

    validations added

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  login!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.CreateFormLogin();
+  }
+  onSubmit(): void {
+    console.log(this.login.value);
+  }
+  private CreateFormLogin() {
+    this.login = this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(6)]),
+      ],
+    });
   }
 
+  get requiredEmail(): boolean {
+    const controller = this.login.get('email');
+    return controller?.invalid && controller?.errors?.required;
+  }
+
+  get invalidEmail(): boolean {
+    const controller = this.login.get('email');
+    return (
+      controller?.invalid &&
+      (controller.dirty || controller.touched) &&
+      controller?.errors?.email
+    );
+  }
+  get requiredPassword(): boolean {
+    const controller = this.login.get('password');
+    return controller?.invalid && controller?.errors?.required;
+  }
+  get errorMinLength(): boolean {
+    const controller = this.login.get('password');
+    return controller?.invalid && controller?.errors?.minlength;
+  }
 }
--- frontend/src/app/login/login.module.ts
+++ frontend/src/app/login/login.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-
+import { MatButtonModule } from '@angular/material/button';
 
 import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule,MatButtonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   exports: [LoginComponent],
 })
 export class LoginModule {}
