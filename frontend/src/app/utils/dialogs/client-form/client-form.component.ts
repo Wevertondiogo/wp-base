f545404df63f86269ea2e1a679e24e7ca520625d
Weverton procópio <wevertonprocopio690@gmail.com>
2021-03-01 12:14:49 -0300

    testando design de cadastro

--- frontend/src/app/utils/dialogs/client-form/client-form.component.ts
+++ frontend/src/app/utils/dialogs/client-form/client-form.component.ts
@@ -1,15 +1,40 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-client-form',
   templateUrl: './client-form.component.html',
-  styleUrls: ['./client-form.component.scss']
+  styleUrls: ['./client-form.component.scss'],
 })
 export class ClientFormComponent implements OnInit {
+  firstSignUp!: FormGroup;
+  secondSignUp!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
+  genders = ['M', 'F'];
 
   ngOnInit(): void {
+    this.firstSignUp = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      birthDate: ['', Validators.required],
+      CPF: ['', Validators.required],
+      RG: ['', Validators.required],
+      cellNumber: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      job: ['', Validators.required],
+      whatsapp: ['', Validators.required],
+      gender: ['', Validators.required],
+    });
+    this.secondSignUp = this.fb.group({
+      address: ['', Validators.required],
+      number: ['', Validators.required],
+      complement: ['', Validators.required],
+      district: ['', Validators.required],
+      city: ['', Validators.required],
+      CEP: ['', Validators.required],
+      maritalState: ['', Validators.required],
+      nationality: ['', Validators.required],
+    });
   }
-
 }
--- frontend/src/app/utils/dialogs/dialogs.module.ts
+++ frontend/src/app/utils/dialogs/dialogs.module.ts
@@ -1,13 +1,37 @@
-import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatDialogModule } from '@angular/material/dialog';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
 
 import { ClientFormComponent } from './client-form/client-form.component';
 
 @NgModule({
   declarations: [ClientFormComponent],
-  imports: [CommonModule, MatDialogModule],
+  imports: [
+    CommonModule,
+
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatStepperModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatSelectModule
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
   exports: [ClientFormComponent],
 })
 export class DialogsModule {}
